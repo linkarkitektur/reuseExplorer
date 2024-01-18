2f55e9f358f6228f277acffee1c6afd8379395c6
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2024-01-18 13:51:48 +0100

    Clean up markov clustering file

--- src/algorithms/markov_clustering.h
+++ src/algorithms/markov_clustering.h
@@ -8,14 +8,10 @@
 #include <fmt/format.h>
 
 
-
-// using SpMat = Eigen::SparseMatrix<scalar_t>; // declares a column-major sparse matrix type of double
-// using Triplet = Eigen::Triplet<scalar_t>;
-
-
 namespace linkml{
 namespace markov_clustering{
 
+/// @brief Helper class for printing messages to the console
 class MessagePrinter
 {
 private:
@@ -28,7 +24,6 @@ public:
     void print(std::string string) { if (enabled) std::cout << string << std::endl; }
 };
 
-    
 /// @brief Version of np.allclose for use with sparse matrices
 /// @tparam ScalarT 
 /// @param a Matrix
@@ -36,7 +31,6 @@ public:
 /// @param rtol Relative tolerance
 /// @param atol Absolute tolerance
 /// @return 
-
 template <class ScalarT>
 bool sparse_allclose(Eigen::SparseMatrix<ScalarT> a, Eigen::SparseMatrix<ScalarT> b, double rtol=1e-5, double atol=1e-8){
 
@@ -66,7 +60,6 @@ bool sparse_allclose(Eigen::SparseMatrix<ScalarT> a, Eigen::SparseMatrix<ScalarT
     // return c.max() <= atol;
 }
 
-
 /// @brief Normalize the columns of the given matrix
 /// @tparam ScalarT 
 /// @param matrix The matrix to be normalized
@@ -91,7 +84,6 @@ Eigen::SparseMatrix<ScalarT> normalize(Eigen::SparseMatrix<ScalarT> matrix){
     return matrix;
 }
 
-
 /// @brief Apply cluster inflation to the given matrix by raising each element to the given power.
 /// @tparam ScalarT 
 /// @param matrix The matrix to be inflated
@@ -109,13 +101,7 @@ Eigen::SparseMatrix<ScalarT> inflate(Eigen::SparseMatrix<ScalarT> matrix, double
 
     return normalize(matrix);
 
-
-    // return normalize(matrix.power(power));
-    // return normalize(np.power(matrix, power))
-
 }
-
-
  
 /// @brief Apply cluster expansion to the given matrix by raising the matrix to the given power.
 /// @tparam ScalarT 
@@ -123,22 +109,16 @@ Eigen::SparseMatrix<ScalarT> inflate(Eigen::SparseMatrix<ScalarT> matrix, double
 /// @param power Cluster expansion parameter
 /// @return The expanded matrix
 template <class ScalarT>
-Eigen::SparseMatrix<ScalarT> expand(Eigen::SparseMatrix<ScalarT> matrix, int power){
+Eigen::SparseMatrix<ScalarT> expand(Eigen::SparseMatrix<ScalarT> matrix, double power){
 
-    if (power == 0){
-        Eigen::SparseMatrix<ScalarT> eye(matrix.rows(), matrix.cols());
-        eye.setIdentity();
-        return eye;
+    for (int k = 0; k < matrix.outerSize(); ++k){
+        for (typename Eigen::SparseMatrix<ScalarT>::InnerIterator it(matrix, k); it; ++it){
+            it.valueRef() = std::pow(it.value(), power);
+        }
     }
-
-    for (int i = 0; i < power - 1; i++ ){
-        matrix = matrix * matrix;
-    }
-
     return matrix;
-    
-}
 
+}
 
 /// @brief Add self-loops to the matrix by setting the diagonal to loop_value
 /// @tparam ScalarT 
@@ -150,16 +130,17 @@ Eigen::SparseMatrix<ScalarT> add_self_loops(Eigen::SparseMatrix<ScalarT> matrix,
 
     assert( matrix.cols() == matrix.rows() && "Error, matrix is not square");
 
-    // Enusre that the has a diagonal
-    matrix += Eigen::VectorXd::Ones(matrix.cols()).template cast<ScalarT>().asDiagonal();
-    
-    //set diagonal to loop_value
-    matrix.diagonal().array() << Eigen::VectorXd::Ones(matrix.cols()).template cast<ScalarT>() * static_cast<ScalarT>(loop_value);
+    auto tripletList = std::vector<Eigen::Triplet<ScalarT>>();
+
+    for (size_t i = 0; i < matrix.cols(); i++)
+        tripletList.push_back(Eigen::Triplet<ScalarT>(i, i, loop_value));
+        // new_matrix[i, i] = loop_value
+
+    matrix.setFromTriplets(tripletList.begin(), tripletList.end());
+
 
     return matrix;
 }
-
-
 
 /// @brief Prune the matrix so that very small edges are removed. The maximum value in each column is never pruned.
 /// @tparam ScalarT 
@@ -167,58 +148,36 @@ Eigen::SparseMatrix<ScalarT> add_self_loops(Eigen::SparseMatrix<ScalarT> matrix,
 /// @param threshold The value below which edges will be removed
 /// @return  The pruned matrix
 template <class ScalarT>
-Eigen::SparseMatrix<ScalarT> prune(Eigen::SparseMatrix<ScalarT> matrix, ScalarT threshold){
+Eigen::SparseMatrix<ScalarT> prune(Eigen::SparseMatrix<ScalarT> matrix, double threshold){
 
-    Eigen::SparseMatrix<ScalarT> pruned = Eigen::SparseMatrix<ScalarT>(matrix);
+    Eigen::SparseMatrix<ScalarT> pruned = Eigen::SparseMatrix<ScalarT>(matrix).pruned(threshold);
 
+    auto num_cols = matrix.cols();
 
-    // Set values below threshold to 0
-    for (int col = 0; col < pruned.outerSize(); ++col){
-        for (typename Eigen::SparseMatrix<ScalarT>::InnerIterator it(pruned, col); it; ++it) {
-            if (it.value() < threshold) {
-                it.valueRef() = 0;
+    std::vector<int> row_indices(num_cols);
+    for (int i = 0; i < num_cols; i++) {
+        int max_row_index = 0;
+        ScalarT max_value = matrix.coeff(0, i);
+        for (int j = 1; j < matrix.rows(); j++) {
+            if (matrix.coeff(j, i) > max_value) {
+                max_row_index = j;
+                max_value = matrix.coeff(j, i);
             }
         }
-    }
-    // Remove 0 values
-    pruned.prune(ScalarT(0));
-
-
-    // if isspmatrix(matrix):
-    //     pruned = dok_matrix(matrix.shape)
-    //     pruned[matrix >= threshold] = matrix[matrix >= threshold]
-    //     pruned = pruned.tocsc()
-    // else:
-    //     pruned = matrix.copy()
-    //     pruned[pruned < threshold] = 0
-
-    // keep max value in each column. same behaviour for dense/sparse
-
-
-    // auto row_indices = matrix.argmax(axis=0).reshape((num_cols,));
-    // pruned[row_indices, col_indices] = matrix[row_indices, col_indices];
-
-    // Keep max value in each column
-    for (int col = 0; col < matrix.outerSize(); ++col){
-        ScalarT maxVal = -std::numeric_limits<ScalarT>::infinity();
-        int maxRow = -1;
-        for (typename Eigen::SparseMatrix<ScalarT>::InnerIterator it(matrix, col); it; ++it) {
-            if (it.value() > maxVal) {
-                maxVal = it.value();
-                maxRow = it.row();
-            }
-        }
-        if (maxRow != -1) {
-            pruned.coeffRef(maxRow, col) = matrix.coeffRef(maxRow, col);
-        }
+        row_indices[i] = max_row_index;
     }
 
-    // // auto row_indices = matrix.argmax(axis=0).reshape((num_cols,));
-    // // pruned[row_indices, col_indices] = matrix[row_indices, col_indices];
+
+    auto tripletList = std::vector<Eigen::Triplet<ScalarT>>();
+
+    for (size_t i = 0; i < num_cols; i++)
+        tripletList.push_back(Eigen::Triplet<ScalarT>(row_indices[i], i, matrix.coeff(row_indices[i], i)));
+
+    pruned.setFromTriplets(tripletList.begin(), tripletList.end());
+
 
     return pruned;
 }
-
 
 /// @brief Check for convergence by determining if matrix1 and matrix2 are approximately equal.
 /// @tparam ScalarT 
@@ -228,14 +187,9 @@ Eigen::SparseMatrix<ScalarT> prune(Eigen::SparseMatrix<ScalarT> matrix, ScalarT 
 template <class ScalarT>
 bool converged(Eigen::SparseMatrix<ScalarT> matrix1, Eigen::SparseMatrix<ScalarT> matrix2){
 
-    // if isspmatrix(matrix1) or isspmatrix(matrix2):
     return sparse_allclose(matrix1, matrix2);
 
-    // return np.allclose(matrix1, matrix2)
 }
-
-
-
 
 /// @brief Run a single iteration (expansion + inflation) of the mcl algorithm
 /// @tparam ScalarT 
@@ -244,7 +198,7 @@ bool converged(Eigen::SparseMatrix<ScalarT> matrix1, Eigen::SparseMatrix<ScalarT
 /// @param inflation Cluster inflation factor
 /// @return 
 template <class ScalarT>
-Eigen::SparseMatrix<ScalarT> iterate(Eigen::SparseMatrix<ScalarT> matrix, int expansion, double inflation){
+Eigen::SparseMatrix<ScalarT> iterate(Eigen::SparseMatrix<ScalarT> matrix, double expansion, double inflation){
 
     // Expansion
     matrix = expand(matrix, expansion);
@@ -255,20 +209,12 @@ Eigen::SparseMatrix<ScalarT> iterate(Eigen::SparseMatrix<ScalarT> matrix, int ex
     return matrix;
 }
 
-
-
-
 /// @brief Retrieve the clusters from the matrix
 /// @tparam ScalarT 
 /// @param matrix The matrix produced by the MCL algorithm
 /// @return A list of tuples where each tuple represents a cluster and contains the indices of the nodes belonging to the cluster
 template <class ScalarT>
 std::set<std::vector<size_t>> get_clusters(Eigen::SparseMatrix<ScalarT> matrix){
-
-    // if not isspmatrix(matrix):
-    //     // cast to sparse so that we don't need to handle different 
-    //     // matrix types
-    //     matrix = csc_matrix(matrix)
 
     // get the attractors - non-zero elements of the matrix diagonal
     auto attractors = std::vector<size_t>();
@@ -282,7 +228,6 @@ std::set<std::vector<size_t>> get_clusters(Eigen::SparseMatrix<ScalarT> matrix){
 
     // the nodes in the same row as each attractor form a cluster
     for (auto attractor : attractors){
-        // auto cluster = std::make_tuple<int>(matrix. getrow(attractor).nonzero()[1].tolist());
         auto cluster = std::vector<size_t>();
         for (int i = 0; i < matrix.cols(); i++)
             if (matrix.coeff(attractor, i) > 0)
@@ -291,11 +236,7 @@ std::set<std::vector<size_t>> get_clusters(Eigen::SparseMatrix<ScalarT> matrix){
     }
 
     return clusters;
-
-    // return sorted(list(clusters));
 }
-
-
 
 /// @brief Perform MCL on the given similarity matrix
 /// @tparam ScalarT 
@@ -310,8 +251,8 @@ std::set<std::vector<size_t>> get_clusters(Eigen::SparseMatrix<ScalarT> matrix){
 /// @param verbose Print extra information to the console
 /// @return The final matrix
 template <class ScalarT>
-Eigen::SparseMatrix<ScalarT> run_mcl(Eigen::SparseMatrix<ScalarT> matrix, int expansion=2, double inflation=2, int loop_value=1,
-            int iterations=100, ScalarT pruning_threshold=0.001, int pruning_frequency=1,
+Eigen::SparseMatrix<ScalarT> run_mcl(Eigen::SparseMatrix<ScalarT> matrix, double expansion=2, double inflation=2, int loop_value=1,
+            int iterations=100, double pruning_threshold=0.001, int pruning_frequency=1,
             int convergence_check_frequency=1, bool verbose=false){
 
     assert(expansion > 1 && "Invalid expansion parameter");
