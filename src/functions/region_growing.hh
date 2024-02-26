64ff953b69b78d17adcc058a54ea2cc19eec1b28
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2024-02-26 08:56:58 +0100

    Region growing

---
+++ src/functions/region_growing.hh
@@ -0,0 +1,13 @@
+#pragma once
+#include <types/point_cloud.hh>
+
+
+namespace linkml {
+    PointCloud::Ptr region_growing(
+        linkml::PointCloud::Ptr cloud, 
+        int minClusterSize = 2*(1/0.02)*(1/0.02), // ca 2sqm in 2cm resolution of point cloud
+        int numberOfNeighbours = 30,
+        float smoothnessThreshold =  3.0 / 180.0 * M_PI,
+        float curvatureThreshold = 0.1
+    );
+} // namespace linkml
