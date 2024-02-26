27acd82854696bdf8003496e9dfe35ce50788192
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2024-02-26 09:00:07 +0100

    Added functions

--- src/algorithms/all.hh
+++ src/algorithms/all.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithms/create_cell_complex.hh>
+#include <algorithms/surface_reconstruction.hh>
 #include <algorithms/fit_plane.hh>
 #include <algorithms/fit_planes.hh>
 #include <algorithms/refine.hh>
--- src/functions/all.hh
+++ src/functions/all.hh
@@ -3,3 +3,9 @@
 #include <functions/progress_bar.hh>
 #include <functions/fit_plane_thorugh_points.hh>
 #include <functions/parse_input_files.hh>
+#include <functions/merge_files.hh>
+#include <functions/polyscope.hh>
+#include <functions/load.hh>
+#include <functions/save.hh>
+#include <functions/filter.hh>
+#include <functions/region_growing.hh>
