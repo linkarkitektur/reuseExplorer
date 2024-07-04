c2190d692aaf8c7221b0d9c999612df73016d33d
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2024-07-04 10:11:29 +0200

    Undoing downsampling changes

--- src/functions/downsample.cpp
+++ src/functions/downsample.cpp
@@ -24,49 +24,83 @@ namespace linkml
         // This has potential to reduce the memory usage.
 
 
-        using PointT = PointCloud::Cloud::PointType;
-        using GridPoint = pcl::PointXYZ;
-        using Grid = pcl::PointCloud<GridPoint>;
+        // using PointT = PointCloud::Cloud::PointType;
+        // using GridPoint = pcl::PointXYZ;
+        // using Grid = pcl::PointCloud<GridPoint>;
 
-        // Create the look up grid
-        Grid::Ptr grid (new Grid);
-        pcl::copyPointCloud(*cloud, *grid);
-        pcl::VoxelGrid<GridPoint> sor;
-        sor.setInputCloud(grid);
-        sor.setLeafSize(leaf_size, leaf_size, leaf_size);
-        sor.filter(*grid);
+        // // Create the look up grid
+        // Grid::Ptr grid (new Grid);
+        // pcl::copyPointCloud(*cloud, *grid);
+        // pcl::VoxelGrid<GridPoint> sor;
+        // sor.setInputCloud(grid);
+        // sor.setLeafSize(leaf_size, leaf_size, leaf_size);
+        // sor.filter(*grid);
 
-        // Create KD-Tree
-        pcl::KdTreeFLANN<PointT>::Ptr tree (new pcl::KdTreeFLANN<PointT>);
-        tree->setInputCloud(cloud);
+        // // Create KD-Tree
+        // pcl::KdTreeFLANN<PointT>::Ptr tree (new pcl::KdTreeFLANN<PointT>);
+        // tree->setInputCloud(cloud);
 
-        // Create the filtered cloud
-        PointCloud::Cloud::Ptr filtered_cloud (new PointCloud::Cloud);
-        filtered_cloud->resize(grid->size());
+        // // Create the filtered cloud
+        // PointCloud::Cloud::Ptr filtered_cloud (new PointCloud::Cloud);
+        // filtered_cloud->resize(grid->size());
 
 
-        // Downsample the point cloud
-        Accumulators<PointCloud::Cloud::PointType>::type acc;
-        pcl::PointXYZ gp;
-        PointT searchPoint;
-        pcl::Indices pointIdxNKNSearch = pcl::Indices();
-        std::vector<float> pointNKNSquaredDistance = std::vector<float>();
+        // // Downsample the point cloud
+        // Accumulators<PointCloud::Cloud::PointType>::type acc;
+        // pcl::PointXYZ gp;
+        // PointT searchPoint;
+        // pcl::Indices pointIdxNKNSearch = pcl::Indices();
+        // std::vector<float> pointNKNSquaredDistance = std::vector<float>();
         
-        #pragma omp parallel for private(searchPoint, gp) shared(grid, cloud, filtered_cloud) firstprivate(acc, pointIdxNKNSearch, pointNKNSquaredDistance)
-        for (size_t i = 0; i < grid->size(); i++){
+        // #pragma omp parallel for private(searchPoint, gp) shared(grid, cloud, filtered_cloud) firstprivate(acc, pointIdxNKNSearch, pointNKNSquaredDistance)
+        // for (size_t i = 0; i < grid->size(); i++){
 
-            gp =  grid->at(i);
-            searchPoint = PointT(gp.x, gp.y, gp.z);
+        //     gp =  grid->at(i);
+        //     searchPoint = PointT(gp.x, gp.y, gp.z);
 
-            if (tree->radiusSearch(searchPoint, leaf_size, pointIdxNKNSearch, pointNKNSquaredDistance) > 0){
+        //     if (tree->radiusSearch(searchPoint, leaf_size, pointIdxNKNSearch, pointNKNSquaredDistance) > 0){
 
-                for (auto const index : pointIdxNKNSearch){
-                    PointCloud::Cloud::PointType point = cloud->at(index);
-                    boost::fusion::for_each (acc, pcl::detail::AddPoint<PointCloud::Cloud::PointType> (point));
-                }
+        //         for (auto const index : pointIdxNKNSearch){
+        //             PointCloud::Cloud::PointType point = cloud->at(index);
+        //             boost::fusion::for_each (acc, pcl::detail::AddPoint<PointCloud::Cloud::PointType> (point));
+        //         }
 
-                boost::fusion::for_each (acc, pcl::detail::GetPoint< PointCloud::Cloud::PointType> (filtered_cloud->at(i), pointIdxNKNSearch.size()));
+        //         boost::fusion::for_each (acc, pcl::detail::GetPoint< PointCloud::Cloud::PointType> (filtered_cloud->at(i), pointIdxNKNSearch.size()));
+        //     }
+        // }
+
+        // std::swap(*cloud, *filtered_cloud);
+
+        pcl::octree::OctreePointCloudPointVector<PointCloud::Cloud::PointType> octree(leaf_size);
+        octree.setInputCloud(cloud);
+        octree.addPointsFromInputCloud();
+
+
+        std::vector<pcl::octree::OctreePointCloudPointVector<PointCloud::Cloud::PointType>::LeafNodeIterator> nodes;
+        for (auto it = octree.leaf_depth_begin(), it_end = octree.leaf_depth_end(); it != it_end; ++it)
+            nodes.push_back(it);
+
+        size_t leaf_count = octree.getLeafCount();
+        PointCloud filtered_cloud = PointCloud();
+        filtered_cloud->resize(leaf_count);
+
+        
+        #pragma omp parallel for shared(filtered_cloud, nodes)
+        for (size_t i = 0; i < leaf_count; i++){
+            pcl::octree::OctreeContainerPointIndices& container = nodes[i].getLeafContainer();
+
+            pcl::Indices indexVector;
+            container.getPointIndices(indexVector);
+
+            Accumulators<PointCloud::Cloud::PointType>::type acc;
+
+            for (auto const& index : indexVector){
+                PointCloud::Cloud::PointType point = cloud->at(index);
+                boost::fusion::for_each (acc, pcl::detail::AddPoint<PointCloud::Cloud::PointType> (point));
             }
+
+            boost::fusion::for_each (acc, pcl::detail::GetPoint< PointCloud::Cloud::PointType> (filtered_cloud->at(i), indexVector.size()));
+
         }
 
         std::swap(*cloud, *filtered_cloud);
